b62cf0d16c30e654bcf0fd423f4e5addebf94545
HH-pro <hamzamanzoor0011@gmail.com>
2022-12-12 13:36:40 +0500

    HH1

--- real/settings.py
+++ real/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'ro@2i^6v+6xm_@9)#^9dt*k%!!7bm%$o^9xvk$i&p+%1jj8_1q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
